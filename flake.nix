9f5f55f2fc62085db85a1134a7c81a7efcfa2fcb
James Guthrie <james@guthrie.ch>
2026-06-15 13:44:53 +0200

    chore: bump version to 0.2.0

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         overlays.default = final: prev: {
           renege = final.rustPlatform.buildRustPackage {
             pname = "renege";
-            version = "0.1.0";
+            version = "0.2.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
             buildInputs = [ final.apple-sdk ];
